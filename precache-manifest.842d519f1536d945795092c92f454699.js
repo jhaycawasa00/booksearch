b13e81770ed1a9018839c31f3c6164d06d73058d
jhay <pipzhermogenes@gmail.com>
2020-01-15 20:18:44 +0800

    Updates

---
+++ precache-manifest.842d519f1536d945795092c92f454699.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8002506ca91f34f3a4ed96e7895a1930",
+    "url": "/booksearch/index.html"
+  },
+  {
+    "revision": "f059c6c0f2d189d9ed20",
+    "url": "/booksearch/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e7b2e3302a5c98f90b13",
+    "url": "/booksearch/static/js/2.223f8122.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/booksearch/static/js/2.223f8122.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f059c6c0f2d189d9ed20",
+    "url": "/booksearch/static/js/main.dd9fd7d5.chunk.js"
+  },
+  {
+    "revision": "a19d69b2fc69e81ab2cd",
+    "url": "/booksearch/static/js/runtime-main.2f1b706d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/booksearch/static/media/logo.5d5d9eef.svg"
+  }
+]);
